5fb6bd625824b92f56a9cb2b0e5b61203cac3072
Maruf <hossain.maruf186@gmail.com>
2026-05-15 23:34:19 -0400

    add cmdfps scaffold with map and camera constants

---
+++ cmdfps.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int nScreenWidth = 120;			// Console Screen Size X (columns)
+int nScreenHeight = 40;			// Console Screen Size Y (rows)
+int nMapWidth = 16;				// World Dimensions
+int nMapHeight = 16;
+
+float fPlayerX = 14.7f;			// Player Start Position
+float fPlayerY = 5.09f;
+float fPlayerA = 0.0f;			// Player Start Rotation
+float fFOV = 3.14159f / 4.0f;	// Field of View
+float fDepth = 16.0f;			// Maximum rendering distance
+float fSpeed = 5.0f;			// Walking Speed
+
+
+int main() {
+
+  // Create Map of world space # = wall block, . = space
+	wstring map;
+	map += L"#########.......";
+	map += L"#...............";
+	map += L"#.......########";
+	map += L"#..............#";
+	map += L"#......##......#";
+	map += L"#......##......#";
+	map += L"#..............#";
+	map += L"###............#";
+	map += L"##.............#";
+	map += L"#......####..###";
+	map += L"#......#.......#";
+	map += L"#......#.......#";
+	map += L"#..............#";
+	map += L"#......#########";
+	map += L"#..............#";
+	map += L"################";
+
+  return 0;
+}
