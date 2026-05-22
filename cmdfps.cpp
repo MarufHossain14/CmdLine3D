d385d0b9f59d9af5ac469fc368e855e03057bb96
Maruf <hossain.maruf186@gmail.com>
2026-05-21 23:16:14 -0400

    Render 2D map to screen buffer

--- cmdfps.cpp
+++ cmdfps.cpp
@@ -10,80 +10,81 @@ screen = what you draw each frame
 hConsole = where you display the screen
 */
 
-int nScreenWidth = 120;			// Console Screen Size X (columns)
-int nScreenHeight = 40;			// Console Screen Size Y (rows)
-int nMapWidth = 16;				// World Dimensions
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+int nMapWidth = 16;
 int nMapHeight = 16;
 
-float fPlayerX = 14.7f;			// Player Start Position
+float fPlayerX = 14.7f;
 float fPlayerY = 5.09f;
-float fPlayerA = 0.0f;			// Player Start Rotation
-float fFOV = 3.14159f / 4.0f;	// Field of View
-float fDepth = 16.0f;			// Maximum rendering distance
-float fSpeed = 5.0f;			// Walking Speed
+float fPlayerA = 0.0f;
+float fFOV = 3.14159f / 4.0f;
+float fDepth = 16.0f;
+float fSpeed = 5.0f;
 
+int main()
+{
+    wstring map;
+    map += L"#########.......";
+    map += L"#...............";
+    map += L"#.......########";
+    map += L"#..............#";
+    map += L"#......##......#";
+    map += L"#......##......#";
+    map += L"#..............#";
+    map += L"###............#";
+    map += L"##.............#";
+    map += L"#......####..###";
+    map += L"#......#.......#";
+    map += L"#......#.......#";
+    map += L"#..............#";
+    map += L"#......#########";
+    map += L"#..............#";
+    map += L"################";
 
-int main() {
+    wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
 
-  // Create Map of world space # = wall block, . = space
-	wstring map;
-	map += L"#########.......";
-	map += L"#...............";
-	map += L"#.......########";
-	map += L"#..............#";
-	map += L"#......##......#";
-	map += L"#......##......#";
-	map += L"#..............#";
-	map += L"###............#";
-	map += L"##.............#";
-	map += L"#......####..###";
-	map += L"#......#.......#";
-	map += L"#......#.......#";
-	map += L"#..............#";
-	map += L"#......#########";
-	map += L"#..............#";
-	map += L"################";
+    HANDLE hConsole = CreateConsoleScreenBuffer(
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        NULL,
+        CONSOLE_TEXTMODE_BUFFER,
+        NULL
+    );
 
+    SetConsoleActiveScreenBuffer(hConsole);
 
-  // Create Screen Buffer
-  wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
-  HANDLE hConsole = CreateConsoleScreenBuffer(
-    GENERIC_READ | GENERIC_WRITE,
-    0,
-    NULL,
-    CONSOLE_TEXTMODE_BUFFER,
-    NULL
-);
+    DWORD dwBytesWritten = 0;
 
-  SetConsoleActiveScreenBuffer(hConsole);
+    while (true)
+    {
+        for (int i = 0; i < nScreenWidth * nScreenHeight; i++)
+            screen[i] = L' ';
 
-  DWORD dwBytesWritten = 0;
+        swprintf_s(screen, 40, L"Hello Raycaster!");
 
-  while (true)
-  {
-      for (int i = 0; i < nScreenWidth * nScreenHeight; i++)
-          screen[i] = L' ';
+        for (int x = 0; x < nMapWidth; x++)
+        {
+            for (int y = 0; y < nMapHeight; y++)
+            {
+                screen[(y + 1) * nScreenWidth + x] =
+                    map[y * nMapWidth + x];
+            }
+        }
 
-      swprintf_s(screen, 40, L"Hello Raycaster!");
+        screen[nScreenWidth * nScreenHeight - 1] = L'\0';
 
-      screen[nScreenWidth * nScreenHeight - 1] = L'\0';
+        WriteConsoleOutputCharacter(
+            hConsole,
+            screen,
+            nScreenWidth * nScreenHeight,
+            { 0, 0 },
+            &dwBytesWritten
+        );
+    }
 
-      WriteConsoleOutputCharacter(
-          hConsole,
-          screen,
-          nScreenWidth * nScreenHeight,
-          { 0, 0 },
-          &dwBytesWritten
-      );
-  }
+    delete[] screen;
+    CloseHandle(hConsole);
 
-  // Clean up
-  delete[] screen;
-  CloseHandle(hConsole);
-
-  // Clean up
-  delete[] screen;
-  CloseHandle(hConsole);
-
-  return 0;
+    return 0;
 }
